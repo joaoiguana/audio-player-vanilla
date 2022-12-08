9265ed5562ec46facd1e9045683a1d5f6577dac2
joao garcia <jiboiadamor@gmail.com>
2022-12-08 16:06:07 +0000

    added event listeners to the player

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const playBtn = document.querySelector('#play');
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
 const audio = document.querySelector('#audio');
-const progress = document.querySelector('.progressive');
-const progressContainer = document.querySelector('.progressive-container');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
 const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
 
@@ -23,3 +23,63 @@ function loadSong(song) {
   audio.src = `music/${song}.mp3`
   cover.src = `images/${song}.jpg`
 }
+
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+
+  audio.pause();
+}
+
+function prevSong() {
+  songIndex--
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1
+  }
+
+  loadSong(songs[songIndex])
+
+  playSong()
+}
+
+function nextSong() {
+  songIndex++
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0
+  }
+
+  loadSong(songs[songIndex])
+
+  playSong()
+}
+
+function updateProgress(e) {
+  const {duration, currentTime} = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Event listeners
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  isPlaying ? pauseSong() : playSong();
+})
+
+// Change song events
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
